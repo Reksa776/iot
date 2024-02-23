056e696aff833d6d52c75f6b79fc76501d3d949d
Reksa776 <116544285+Reksa776@users.noreply.github.com>
2024-02-23 10:55:11 +0700

    Update pengirim.ino

--- project now/pengirim.ino
+++ project now/pengirim.ino
@@ -2,31 +2,26 @@
 #include <MFRC522.h>
  #include <ESP32Servo.h> 
  #include <Wire.h> 
- #include <esp_now.h>
 #include <WiFi.h>
 #include <LiquidCrystal_I2C.h>
 #include <lorawan.h>
 #include "Arduino.h"
+#include "DFRobotDFPlayerMini.h"
+#include <SoftwareSerial.h>
 #include <Ultrasonic.h>
+#include <AntaresESPHTTP.h>     // Include the AntaresESP32HTTP library
+
+#define ACCESSKEY "03fd039bd6554101:c3be4620d72ab8bb"       // Replace with your Antares account access key
+#define WIFISSID "Bandits of Bandwidth"         // Replace with your Wi-Fi SSID
+#define PASSWORD "qwerty123"     // Replace with your Wi-Fi password
+
+#define projectName "smartGateParking"          // Antares project name
+#define deviceNameSensor "sensorDIgital"   // Name of the device sending sensor data
+
+AntaresESPHTTP antares(ACCESSKEY);   // Initialize AntaresESP32HTTP with the access key
 
 
-// mac address tujuan
-uint8_t mac_addr_tujuan[] = {0xc8, 0xc9, 0xa3, 0xeb, 0xca, 0xb4};
 
-// struktur data yang dikirim
-typedef struct struct_message {
-  int nilai;
-} struct_message;
-struct_message data_ku;
-
-// variabel peerInfo
-esp_now_peer_info_t peerInfo;
-
-// callback Jika data terkirim
-void cb_terkirim(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nStatus Pengiriman Paket Terakhir:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Pengiriman Sukses" : "Pengiriman Gagal");
-}
 
 //ABP Credentials
 /*
@@ -40,8 +35,12 @@ const char *devAddr = "89eb8336"; // Replace with the Device Address that you ha
 const char *nwkSKey = "73a7f04d15eecf7ca0143e1156a04eb8"; // Replace with the Network Session Key that you have in the Antares console
 const char *appSKey = "392e8b3a561a2f806e36cf2d312a56f8"; // Replace with the Application Session Key that you have in the Antares console
 
-const unsigned long interval = 10000;    // 10 s interval to send message
-unsigned long previousMillis = 0;  // will store last time message sent
+const unsigned long interval = 5000;     // Interval to send sensor data (5 seconds)
+const unsigned long interval2 = 10000;   // Interval to get data from Antares (10 seconds)
+unsigned long previousMillis = 0;        // Store the last time sensor data was sent
+unsigned long previousMillis2 = 0;       // Store the last time data was received from Antares
+
+
 unsigned int counter = 0;     // message counter
 
 char myStr[50];
@@ -75,14 +74,28 @@ const sRFM_pins RFM_pins = {
    or only the signal pin (for sensors 3 pins), like:
    Ultrasonic ultrasonic(13);
 */
+
+#if (defined(ARDUINO_AVR_UNO) || defined(ESP8266))   // Using a soft serial port
+#include <SoftwareSerial.h>
+SoftwareSerial softSerial(/*rx =*/26, /*tx =*/25);
+#define FPSerial softSerial
+#else
+#define FPSerial Serial1
+#endif
+DFRobotDFPlayerMini myDFPlayer;
+
+
 // Ultrasonic ultrasonic(26,25);
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 LiquidCrystal_I2C lcd(0x27,16,2);
 Servo myservo;
 int distance;
 
-int echo = 25;
-int trig = 26;
+String testData;            // String to hold received data from Antares
+String lastData="";         // String to store last received data
+
+int echo = 33;
+int trig = 32;
 long jarak,durasi;
 void setup() {
   #if defined (ARDUINO_B_L072Z_LRWAN1)
@@ -119,28 +132,29 @@ void setup() {
   lora.setNwkSKey(nwkSKey);
   lora.setAppSKey(appSKey);
   lora.setDevAddr(devAddr);
+
+  #if (defined ESP32)
+  FPSerial.begin(9600, SERIAL_8N1, /*rx =*/26, /*tx =*/25);
+#else
+  FPSerial.begin(9600);
+#endif
+
+  antares.setDebug(true);    // Enable Antares library debug mode
+  antares.wifiConnection(WIFISSID, PASSWORD);  // Connect to WiFi using provided SSID and password
   // put your setup code here, to run once:
     // mengatur esp ke mode station
   WiFi.mode(WIFI_STA);
   SPI.begin();          // Initiate  SPI bus
-   // inisialisasi espnow
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Gagal Inisialisasi espnow");
-    return;
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */true)) {  //Use serial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
   }
-  // mendaftarkan fungsi callback  
-  esp_now_register_send_cb(cb_terkirim);
-  
-  // Mendata Peer / Kawan
-  memcpy(peerInfo.peer_addr, mac_addr_tujuan, 6);
-  peerInfo.channel = 0;  
-  peerInfo.encrypt = false;
-  
-  // Menambahkan Peer / Kawan
-  if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Gagal Menambah Peer");
-    return;
-  }
+  myDFPlayer.play(3);
+  myDFPlayer.volume(100);
   mfrc522.PCD_Init();
   Serial.println("Put your card to the reader...");
   Serial.println();
@@ -156,7 +170,7 @@ void setup() {
   lcd.print("Tempelkan Kartu!");
   Serial.println(F("DFRobot DFPlayer Mini Demo"));
   Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
-
+  myservo.write(178);
   
 
 }
@@ -201,52 +215,93 @@ void loop() {
   delay(5);
   
   if(jarak <= 5 ){
-    if (content.substring(1) == "53 41 03 AA") //TUKAR UID INI SESUAI RFIA TAG KALIAN
-    {
-      data_ku.nilai = 1;
-      // mengirim data
-      esp_err_t result = esp_now_send(mac_addr_tujuan, (uint8_t *) &data_ku, sizeof(data_ku));
-
-      // status pengiriman
-      if (result == ESP_OK) {
-        Serial.println("Berhasil");
-      }
-      else {
-        Serial.println("Gagal");
-      }
-      
-      lcd.setCursor(0,1);
-      lcd.print("Silahkan Masuk!        ");
-        for(pos = 5; pos < 98; pos += 1)  //fungsi perulangan yang akan dijadikan PWM dengan kenaikan 1
-        {
-          myservo.write(pos); //prosedur penulisan data PWM ke motor servo
-          delay(15); //waktu tunda 15 ms                 
-        } 
-        delay(2000);
-        for(pos = 98; pos>=5; pos-=1)  //fungsi perulangan yang akan dijadikan PWM dengan penurunan 1
+      if (content.substring(1) == "53 41 03 AA"){
+          antares.add("user", "reksa");                    // Add temperature data to 
+          antares.send(projectName, deviceNameSensor);
+          
+          myDFPlayer.play(2);
+          lcd.setCursor(0,1);
+          lcd.print("Silahkan Masuk!        ");
+            for(pos = 178; pos >= 98; pos -= 1)  //fungsi perulangan yang akan dijadikan PWM dengan kenaikan 1
+            {
+              myservo.write(pos); //prosedur penulisan data PWM ke motor servo
+              delay(15); //waktu tunda 15 ms                 
+            } 
+          delay(2000);
+          antares.add("user", "none");                    // Add temperature data to 
+          antares.send(projectName, deviceNameSensor);
+          while(jarak >= 5){
+            myservo.write(pos);
+          }
+          for(pos = 98; pos <= 178; pos+=1)  //fungsi perulangan yang akan dijadikan PWM dengan penurunan 1
         {                              
           myservo.write(pos);                 
           delay(15);                        
         }
         lcd.setCursor(0,1);
         lcd.print("Tempelkan Kartu!");
-    }
+        }else if (content.substring(1) == "53 77 8A 0D"){
+          antares.add("user", "Agus");                    // Add temperature data to 
+          antares.send(projectName, deviceNameSensor);
+          
+          myDFPlayer.play(2);
+          lcd.setCursor(0,1);
+          lcd.print("Silahkan Masuk!        ");
+            for(pos = 178; pos >= 98; pos -= 1)  //fungsi perulangan yang akan dijadikan PWM dengan kenaikan 1
+            {
+              myservo.write(pos); //prosedur penulisan data PWM ke motor servo
+              delay(15); //waktu tunda 15 ms                 
+            } 
+          delay(2000);
+          antares.add("user", "none");                    // Add temperature data to 
+          antares.send(projectName, deviceNameSensor);
+          do{
+            myservo.write(pos);
+          }while((jarak <= 5))
+          for(pos = 98; pos <= 178; pos+=1)  //fungsi perulangan yang akan dijadikan PWM dengan penurunan 1
+        {                              
+          myservo.write(pos);                 
+          delay(15);                        
+        }
+        lcd.setCursor(0,1);
+        lcd.print("Tempelkan Kartu!");
+        }else if (content.substring(1) == "93 A9 2E 10"){
+          antares.add("user", "Algi");                    // Add temperature data to 
+          antares.send(projectName, deviceNameSensor);
+          
+          myDFPlayer.play(2);
+          lcd.setCursor(0,1);
+          lcd.print("Silahkan Masuk!        ");
+            for(pos = 178; pos >= 98; pos -= 1)  //fungsi perulangan yang akan dijadikan PWM dengan kenaikan 1
+            {
+              myservo.write(pos); //prosedur penulisan data PWM ke motor servo
+              delay(15); //waktu tunda 15 ms                 
+            } 
+          delay(2000);
+          antares.add("user", "none");                    // Add temperature data to 
+          antares.send(projectName, deviceNameSensor);
+          while(jarak >= 5){
+            myservo.write(pos);
+          }
+          for(pos = 98; pos <= 178; pos+=1)  //fungsi perulangan yang akan dijadikan PWM dengan penurunan 1
+        {                              
+          myservo.write(pos);                 
+          delay(15);                        
+        }
+        lcd.setCursor(0,1);
+        lcd.print("Tempelkan Kartu!");
+        }
   
     else   {
-      data_ku.nilai = 0;
-      // mengirim data
-      esp_err_t result = esp_now_send(mac_addr_tujuan, (uint8_t *) &data_ku, sizeof(data_ku));
-
-      // status pengiriman
-      if (result == ESP_OK) {
-        Serial.println("Berhasil");
-      }
-      else {
-        Serial.println("Gagal");
-      }
+      antares.add("user", "Unknow");                    // Add temperature data to 
+      antares.send(projectName, deviceNameSensor);
+      
+      myDFPlayer.play(1);
       lcd.setCursor(0,1);
       lcd.print("Akses Ditolak!      ");
       delay(2000);
+      antares.add("user", "none");                    // Add temperature data to 
+      antares.send(projectName, deviceNameSensor);
     }
   }else{
     lcd.setCursor(0,1);
